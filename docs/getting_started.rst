e877afe04ce9a52fc0aa88e6ee213048e24fb43f
KubaTaba1uga <luna7337anul@proton.me>
2025-12-10 13:35:11 +0100

    add simple defcondifg

--- docs/getting_started.rst
+++ docs/getting_started.rst
@@ -4,15 +4,33 @@ Getting Started
 Installation
 ------------
 
+If you do not have `invoke` installed use:
+
+.. code-block:: console
+
+   $ apt-get install python3-invoke
+
+Once `invoke` is installed download the repository and install dependencies:
+   
 .. code-block:: console
 
    $ git clone https://github.com/you/stm32mp135f_ebook_reader.git
    $ cd stm32mp135f_ebook_reader
    $ inv install
-   $ inv build-docs
-   $ inv serve-docs   
 
+  
 Usage
 -----
 
-Describe how to run or build your project here.
+Currently we support one build called `ebook_reader_dev_defconfig`, which is meant for quick development cycle.
+
+First we need to download a sources required for our config:
+
+.. code-block:: console
+
+   $ inv configure
+   $ inv download
+
+Now all required packages are downloaded on the disk in `build/third_party` directory.
+
+Once packages are downloaded we are ready for 
--- tasks.py
+++ tasks.py
@@ -28,10 +28,12 @@ def add_repo(c, name, tag, url):
 def install(c):
     _pr_info(f"Installing dependencies...")
 
-
-    
     try: 
-       c.run("sudo apt-get install doxygen")
+       c.run("sudo apt-get install -y doxygen \
+              which sed make binutils build-essential diffutils \
+              gcc g++ bash patch gzip bzip2 perl tar cpio \
+              unzip rsync file bc findutils gawk wget \
+              git libncurses5-dev python3")
        c.run("virtualenv .venv")
        c.run("pip install sphinx==8.2.3 breathe==4.36.0 furo==2025.9.25 sphinx-autobuild==2025.08.25")
     except Exception:
@@ -40,7 +42,6 @@ def install(c):
 
     _pr_info(f"Installing dependencies completed")
     
-
 @task
 def build_docs(c):
     _pr_info(f"Building docs...")
@@ -58,7 +59,32 @@ def build_docs(c):
     _pr_info(f"Building docs completed")
 
 @task
-def build_linux(c):
+def configure(c, config="ebook_reader_dev_defconfig"):
+    _pr_info(f"Configuring buildroot...")    
+
+    with c.cd("third_party/buildroot"):
+        flags = ["O=../../build/buildroot", "BR2_EXTERNAL=../../br2_external", config]
+        
+        c.run(f"make " + " ".join(flags))
+        
+    _pr_info(f"Configuring buildroot completed")
+
+    
+@task
+def download(c, config="ebook_reader_dev_defconfig"):
+    _pr_info(f"Downloading dependencies...")    
+
+    if config:
+        configure(c, config)
+    
+    with c.cd("build/buildroot"):
+         c.run("make BR2_DL_DIR=../../build/third_party source")
+         
+    _pr_info(f"Downloading dependencies completed")
+
+    
+@task
+def build_linux(c, config="ebook_reader_dev_defconfig"):
     _pr_info(f"Building linux...")
 
     with c.cd("buildroot"):
@@ -67,7 +93,7 @@ def build_linux(c):
     _pr_info(f"Building linux completed")
 
 @task
-def build_uboot(c):
+def build_uboot(c, config="ebook_reader_dev_defconfig"):
     _pr_info(f"Building u-boot...")
 
     with c.cd("buildroot"):
@@ -76,7 +102,7 @@ def build_uboot(c):
     _pr_info(f"Building u-boot completed")
 
 @task
-def build_tfa(c):
+def build_tfa(c, config="ebook_reader_dev_defconfig"):
     _pr_info(f"Building tf-a...")
 
     with c.cd("buildroot"):
@@ -85,20 +111,15 @@ def build_tfa(c):
     _pr_info(f"Building tf-a completed")
 
     
-    
-    
 @task
-def build_bsp(c, configure=True):
+def build_bsp(c, config="ebook_reader_dev_defconfig"):
     _pr_info(f"Building BSP...")    
 
-    if configure:
-       with c.cd("buildroot"):
-           c.run("make O=../build/buildroot \
-                       BR2_DL_DIR=../third_party \
-                       BR2_EXTERNAL=../buildroot-external-st \
-                       st_stm32mp135f_dk_defconfig")
+    if config:
+       configure(c, config)
+           
     with c.cd("build/buildroot"):
-        c.run("make")
+        c.run("make BR2_DL_DIR=../../build/third_party")
 
     _pr_info(f"Building BSP completed")
     
