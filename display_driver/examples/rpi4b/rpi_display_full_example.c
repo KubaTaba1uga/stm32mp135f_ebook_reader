bc8f4735f60a2c21e82fdb1d4cc9b76a5563fccc
KubaTaba1uga <luna7337anul@proton.me>
2025-12-26 23:43:09 +0100

    add working example showing how display your own image

--- display_driver/examples/rpi4b/rpi_display_full_example.c
+++ display_driver/examples/rpi4b/rpi_display_full_example.c
@@ -1,15 +1,52 @@
 #define _POSIX_C_SOURCE 199309L
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "board.h"
 #include "display_driver.h"
 #include "picture.h"
 
+static void usage(const char *prog) {
+  fprintf(stderr,
+          "Usage: %s [--wvs | --turtle | --cat_sm] [--help]\n"
+          "\n"
+          "Display a built-in image on the 7.5\" V2 e-paper panel (480x800).\n"
+          "\n"
+          "Options:\n"
+          "  --wvs       Show image_7in5_v2\n"
+          "  --turtle    Show turtle_7in5_v2\n"
+          "  --cat_sm    Show cat_sm_7in5_v2\n"
+          "  -h, --help  Show this help and exit\n",
+          prog);
+}
 
-int main(void) {
+int main(int argc, char *argv[]) {
+  unsigned char *buf = (unsigned char *)turtle_7in5_v2;
+  int buf_len = sizeof(turtle_7in5_v2);;
   dd_image_t img;
   dd_error_t err;
+
+  /* classic argv parsing */
+  for (int i = 1; i < argc; ++i) {
+    if (strcmp(argv[i], "--wvs") == 0) {
+      buf = (unsigned char *)image_7in5_v2;
+      buf_len = sizeof(image_7in5_v2);
+    } else if (strcmp(argv[i], "--turtle") == 0) {
+      buf = (unsigned char *)turtle_7in5_v2;
+      buf_len = sizeof(turtle_7in5_v2);
+    } else if (strcmp(argv[i], "--cat_sm") == 0) {
+      buf = (unsigned char *)cat_sm_7in5_v2;
+      buf_len = sizeof(cat_sm_7in5_v2);
+    } else if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) {
+      usage(argv[0]);
+      return EXIT_SUCCESS;
+    } else {
+      fprintf(stderr, "Unknown option: %s\n", argv[i]);
+      usage(argv[0]);
+      return EXIT_FAILURE;
+    }
+  }
 
   puts("Starting driver");
   err = init_rpi4b();
@@ -28,27 +65,12 @@ int main(void) {
     goto error_dd_cleanup;
   }
 
-  /* err = */
-  /*     dd_image_init(&img, (unsigned char *)image_7in5_v2, sizeof(image_7in5_v2), */
-  /*                   (struct dd_ImagePoint){.x = 480, .y = 800}); */
-  /* if (err) { */
-  /*   goto error_dd_cleanup; */
-  /* } */
-
-  /* err = */
-  /*     dd_image_init(&img, (unsigned char *)turtle_7in5_v2, sizeof(image_7in5_v2), */
-  /*                   (struct dd_ImagePoint){.x = 480, .y = 800}); */
-  /* if (err) { */
-  /*   goto error_dd_cleanup; */
-  /* } */
-  err =
-      dd_image_init(&img, (unsigned char *)super, sizeof(image_7in5_v2),
-                    (struct dd_ImagePoint){.x = 480, .y = 800});
+  err = dd_image_init(&img, buf, buf_len,
+                      (struct dd_ImagePoint){.x = 480, .y = 800});
   if (err) {
     goto error_dd_cleanup;
   }
 
-  
   err = dd_wvs75v2b_ops_display_full(dd, img);
   if (err) {
     goto error_img_cleanup;
