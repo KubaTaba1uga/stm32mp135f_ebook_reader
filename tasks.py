0631a7f8374083d577ebcf134ba7158a44d5438e
KubaTaba1uga <luna7337anul@proton.me>
2025-12-27 17:52:24 +0100

    WIP

--- tasks.py
+++ tasks.py
@@ -1,5 +1,5 @@
-import os
 import glob
+import os
 
 from invoke import task
 
@@ -7,21 +7,21 @@ ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 BUILD_PATH = os.path.join(ROOT_PATH, "build")
 DOCS_PATH = os.path.join(ROOT_PATH, "docs")
 
-os.environ["PATH"] = f'{os.path.join(ROOT_PATH, ".venv", "bin")}:{os.environ["PATH"]}'
+os.environ["PATH"] = f"{os.path.join(ROOT_PATH, '.venv', 'bin')}:{os.environ['PATH']}"
 
 @task
 def add_repo(c, name, tag, url):
-    c.run( "mkdir -p third_party")
-    c.run( "git status")
-    c.run( "git add . || true")
-    c.run( "git commit -m \"WIP\" || true")
+    c.run("mkdir -p third_party")
+    c.run("git status")
+    c.run("git add . || true")
+    c.run('git commit -m "WIP" || true')
 
-    if c.run(f"git remote get-url \"{name}\"", warn=True, echo=False) == 0:
+    if c.run(f'git remote get-url "{name}"', warn=True, echo=False) == 0:
         _pr_error(f"{name} already exists")
         return 1
 
-    c.run( f"git remote add \"{name}\" \"{url}\"")
-    c.run( f"git subtree add --prefix=\"third_party/{name}\" \"{name}\" \"{tag}\" --squash")
+    c.run(f'git remote add "{name}" "{url}"')
+    c.run(f'git subtree add --prefix="third_party/{name}" "{name}" "{tag}" --squash')
 
 
 ###############################################
@@ -41,7 +41,4 @@ def _pr_debug(message: str):
 
 def _pr_error(message: str):
     print(f"\033[91m[ERROR] {message}\033[0m")
-        
 
-
-    
