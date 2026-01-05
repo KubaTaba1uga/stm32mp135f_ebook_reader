c0e9abf379f2489c7e70a216ec96bb503e657e6b
KubaTaba1uga <luna7337anul@proton.me>
2026-01-05 12:40:41 +0100

    rotate is working for 2x2

--- display_driver/test/test_rotate.c
+++ display_driver/test/test_rotate.c
@@ -30,6 +30,28 @@ int get_bit(int i, unsigned char *buf, uint32_t buf_len) {
   return (buf[byte_number] & (1 << bit_number)) > 0;
 }
 
+void set_bit(int i, int val, unsigned char *buf, uint32_t buf_len) {
+  int byte_number = floor(i / 8);
+
+  if (byte_number < 0 || byte_number >= buf_len) {
+    return;
+  }
+
+  // If MSB
+  int bit_number = 7 - (i % 8);
+  // If LSB
+  /* int bit_number = ((i) % 8) - 1; */
+  if (bit_number < 0) {
+    bit_number = 0;
+  }
+
+  if (val == 0) {
+    buf[byte_number] &= ~(1 << bit_number);
+  } else {
+    buf[byte_number] |= (1 << bit_number);
+  }
+}
+
 int get_pixel(int x, int y, int width, unsigned char *buf, uint32_t buf_len) {
   if (x < 0 || y < 0) {
     return -1;
@@ -40,35 +62,57 @@ int get_pixel(int x, int y, int width, unsigned char *buf, uint32_t buf_len) {
   return get_bit(bit, buf, buf_len);
 }
 
+void set_pixel(int x, int y, int width, int val, unsigned char *buf,
+               uint32_t buf_len) {
+  if (x < 0 || y < 0) {
+    return;
+  }
+
+  int bit = width * y + x;
+
+  set_bit(bit, val, buf, buf_len);
+}
+
 int rotate(int width, int heigth, unsigned char *buf, uint32_t buf_len) {
-  char *rotated = malloc(buf_len);
+  unsigned char *rotated = malloc(buf_len);
   TEST_ASSERT(rotated != NULL);
-  char *new_img = rotated;
+  /* unsigned char *new_img = rotated; */
   memset(rotated, 0, buf_len);
 
-  /* int first_pixel = width * (heigth - 1); */
-  /* int last_pixel = first_pixel + width; */
+  int first_pixel = width * (heigth - 1);
+  int last_pixel = first_pixel + width;
 
-  /* int rows = 0; */
-  /* int columns = 0; */
+  int rows = 0;
+  int columns = 0;
+  (void)rows;
+  (void)columns;
 
-  unsigned char byte = 0;
-  for (int x = width - 1; x > 0; x--) {
-    int bit = 0;
-    for (int y = 0; y < heigth; y++) {
-      byte |= get_pixel(x, y, width, buf, buf_len) << bit;
-      if (bit == 7) {
-        bit = 0;
-        *new_img = byte;
-        new_img++;
-      }
-
-      bit++;
-      /* columns++; */
+  for (int src_i = first_pixel, dst_i = 0; src_i < last_pixel;
+       src_i++) {
+    for (int src_k = 0; src_k < heigth; src_k++, dst_i++) {
+      int bit_number = src_i - (width * src_k);
+      int bit_value = get_bit(bit_number, buf, buf_len);
+      set_bit(dst_i, bit_value, rotated, buf_len);
     }
-    /* columns = 0; */
-    /* rows++; */
   }
+  /*   for (int y = 0; y < heigth; y++) { */
+
+  /* for (int x = 0, xcp = 0; x > 0; x++, xcp++) { */
+  /*   for (int y = 0; y < heigth; y++) { */
+
+  /* for (int x = width - 1, xcp = 0; x > 0; x--, xcp++) { */
+  /*   for (int y = 0; y < heigth; y++) { */
+  /*     set_pixel(xcp, y, width, get_pixel(x, y, width, buf, buf_len), rotated,
+   */
+  /*               buf_len); */
+
+  /*     printf("wrote %dx%d=%d\n", rows, columns, */
+  /*            get_pixel(x, y, width, buf, buf_len)); */
+  /*     columns++; */
+  /*   } */
+  /*   columns = 0; */
+  /*   rows++; */
+  /* } */
 
   /* TEST_ASSERT(last_pixel - first_pixel == width); */
   /* // We are iterating pixels */
@@ -106,34 +150,63 @@ int rotate(int width, int heigth, unsigned char *buf, uint32_t buf_len) {
 
 void test_rotate(void) {
   int buf2x2w = 16;
-  int buf2x2h = 4;
+  int buf2x2h = 8;
   unsigned char buf2x2[] = {
+      // 2  bytes x 8  = 16  bytes
+      // 16 bits  x 8  = 128 bits
+      0b00000000, 0b00010000, //
+      0b00000000, 0b00010000, //
+      0b00000000, 0b00010000, //
+      0b00000000, 0b00010000, //
+      0b00000000, 0b00000000, //
+      0b00000000, 0b00000000, //
+      0b00000000, 0b00000000, //
+      0b00000000, 0b00000000, //
+  };
+
+  unsigned char buf2x2roatetd[] = {
       // 2  bytes x 2  = 4  bytes
       // 16 bits  x 2  = 32 bits
-      0b00000000, 0b00010000, //
-      0b00000000, 0b00010000, //
-      0b00000000, 0b00010000, //
-      0b00000000, 0b00010000, //
+      0b00000000, //
+      0b00000000, //
+      0b00000000, //
+      0b00000000, //
+      0b11110000, //
+      0b00000000, //
+      0b00000000, //
+      0b00000000, //
+      0b00000000, //
+      0b00000000, //
+      0b00000000, //
+      0b00000000, //
+      0b00000000, //
+      0b00000000, //
+      0b00000000, //
+      0b00000000, //
   };
+
   /* memset(buf2x2, 0, sizeof(buf2x2)); */
 
   print_bits_grid(buf2x2, sizeof(buf2x2), buf2x2w);
+  puts("");
+  
+  int err = rotate(buf2x2w, buf2x2h, buf2x2, sizeof(buf2x2));
+  TEST_ASSERT_EQUAL(0, err);
 
-  /* int err = rotate(16, 2, buf2x2, sizeof(buf2x2)); */
-  /* TEST_ASSERT_EQUAL(0, err); */
+  print_bits_grid(buf2x2, sizeof(buf2x2), buf2x2h);
+  puts("");
+  print_bits_grid(buf2x2roatetd, sizeof(buf2x2roatetd), buf2x2h);
 
-  /* print_bits_grid(buf2x2, sizeof(buf2x2), buf2x2w); */
-
-  for (int y = 0; y < buf2x2h; y++) {
-    for (int x = 0; x < buf2x2w;) {
-      printf("%d", get_pixel(x, y, buf2x2w, buf2x2,sizeof(buf2x2)));
-      x++;
-      if (x > 0 && x % 8 == 0) {
-        printf(", ");
-      }
-    }
-    printf("\n");
-  }
+  /* for (int y = 0; y < buf2x2h; y++) { */
+  /*   for (int x = 0; x < buf2x2w;) { */
+  /*     printf("%d", get_pixel(x, y, buf2x2w, buf2x2,sizeof(buf2x2))); */
+  /*     x++; */
+  /*     if (x > 0 && x % 8 == 0) { */
+  /*       printf(", "); */
+  /*     } */
+  /*   } */
+  /*   printf("\n"); */
+  /* } */
   /* TEST_ASSERT_EQUAL_CHAR_ARRAY(turtle_vertical, turtle_horizontal, */
   /*                              sizeof(turtle_vertical)); */
 
