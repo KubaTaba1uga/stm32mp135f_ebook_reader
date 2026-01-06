9d0c98c2e13fff917460b9669d9c171ba21c76d2
KubaTaba1uga <luna7337anul@proton.me>
2026-01-06 21:25:22 +0100

    cleanup

--- ebook_reader/src/gui/lv/display/wvs7in5v2b.h
+++
@@ -1,7 +0,0 @@
-#ifndef EBOOK_READER_LVGL_DRIVER_WVS7IN5V2B_H
-#define EBOOK_READER_LVGL_DRIVER_WVS7IN5V2B_H
-#include "gui/lv/display/driver.h"
-
-bool lvgl_display_driver_wvs7in5v2b_probe(struct LvglDisplayDriver *out);
-
-#endif // EBOOK_READER_LVGL_DRIVER_WVS7IN5V2B_H
