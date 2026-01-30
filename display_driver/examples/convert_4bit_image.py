f31af7ab6a6d612cf526556084332ad6b292fec5
KubaTaba1uga <luna7337anul@proton.me>
2026-01-30 12:48:41 +0100

    improve rotate

--- display_driver/examples/convert_4bit_image.py
+++ display_driver/examples/convert_4bit_image.py
@@ -17,61 +17,71 @@ Usage:
 
 from PIL import Image
 import argparse
-
+import random
 
 class Pixel:
-    def __init__(self, x, y, rgb):
+    def __init__(self, x, y, rgb, wages):
         self.x = x
         self.y = y
 
         # map so 0b00=white ... 0b11=black
-        if any(val > 230 for val in rgb[0:3]):
+        if any(val > wages[0] for val in rgb[0:3]):
             colour = 0b00
-        elif any(val > 200 for val in rgb[0:3]):
+        elif any(val > wages[1] for val in rgb[0:3]):
             colour = 0b01
-        elif any(val > 140 for val in rgb[0:3]):
+        elif any(val > wages[2] for val in rgb[0:3]):
             colour = 0b10
         else:
             colour = 0b11
-        
+
         self.colour = colour
 
     def __str__(self):
         return f"x={self.x} y={self.y} colour={self.colour}"
 
-    
-    
+
 def main():
     ap = argparse.ArgumentParser(description="Convert BW image to packed C byte array")
     ap.add_argument("input", help="Input image path (png, jpg, ...)")
     ap.add_argument("output", help="Output .c file path")
     ap.add_argument("--bit-order", default="MSB", help="Output bit order")
     ap.add_argument("--name", default="output", help="Name of generated variable")
-    ap.add_argument("--rotate", default=False, help="Rotate")    
+    ap.add_argument("--rotate", default=False, help="Rotate")
     args = ap.parse_args()
 
     img = Image.open(args.input).convert("RGBA")
     width, height = img.size
     pix = img.load()
 
+    wages = set()
+    for y in range(height):
+        for x in range(width):
+            v = pix[x, y]
+            
+            wages.add(max(v[0:2]))
+
+            
+    wages = list(random.sample(sorted(wages), 3))
+    print(wages)
+    
     pixels = []
     colours = set()
 
     if not args.rotate:
-       for y in range(height):
-           for x in range(width):
-               v = pix[x, y]  # 0=black .. 255=white
-               pixel = Pixel(x, y, v)
-               pixels.append(pixel)
-               colours.add(pixel.colour)
+        for y in range(height):
+            for x in range(width):
+                v = pix[x, y]  # 0=black .. 255=white
+                pixel = Pixel(x, y, v, wages)
+                pixels.append(pixel)
+                colours.add(pixel.colour)
     else:
-        for x in range(width-1, 0, -1):
+        for x in range(width - 1, 0, -1):
             for y in range(0, height, 1):
-               v = pix[x, y]  # 0=black .. 255=white
-               pixel = Pixel(x, y, v)
-               pixels.append(pixel)
-               colours.add(pixel.colour)
-               
+                v = pix[x, y]  # 0=black .. 255=white
+                pixel = Pixel(x, y, v, wages)
+                pixels.append(pixel)
+                colours.add(pixel.colour)
+
     print(colours)
     with open(args.output, "w") as fp:
         fp.write(f"unsigned char {args.name}[] = {{")
