d1bfd1f9db824b9260e354d76c1ffc2e4672e880
KubaTaba1uga <luna7337anul@proton.me>
2026-03-11 18:32:10 +0100

    WIP: cleanup

--- ebook_reader/subprojects/IT8951-ePaper/Raspberry/examples/refresh.c
+++ ebook_reader/subprojects/IT8951-ePaper/Raspberry/examples/refresh.c
@@ -210,149 +210,32 @@ int main(int argc, char *argv[]) {
   return 0;
 }
 
-/* static inline int dd_graphic_get_bit(int i, unsigned char *buf, */
-/*                                      uint32_t buf_len) { */
-/*   if (i < 0 || (uint32_t)i >= buf_len * 8) { */
-/*     return -1; */
-/*   } */
-/*   int byte = i >> 3; // same as i/8 but faster   */
-/*   /\* int byte = i / 8; *\/ */
-/*   /\* int bit = 7 - (i % 8); *\/ */
-/*   int bit = (i % 8); */
-/*   return (buf[byte] >> bit) & 1; */
-/* } */
-
-/* static inline void dd_graphic_set_bit(int i, int val, unsigned char *buf, */
-/*                                       uint32_t buf_len) { */
-/*   if (!val) { */
-/*     return; */
-/*   }     */
-/*   int byte = i >> 3; // same as i/8 but faster */
-/*   /\* int bit = 7 - (i % 8); *\/ */
-/*   int bit = (i % 8); */
-
-/*   buf[byte] |= (1u << bit); */
-/* } */
-
-/* static inline int dd_graphic_get_pixel(int x, int y, int width, */
-/*                                        unsigned char *buf, uint32_t buf_len)
- * { */
-/*   if (x < 0 || y < 0) { */
-/*     return -1; */
-/*   } */
-
-/*   int bit = width * y + x; */
-
-/*   return dd_graphic_get_bit(bit, buf, buf_len); */
-/* } */
-
-/* static unsigned char *dd_wvs75v2b_rotate(int width, int heigth, */
-/*                                          unsigned char *buf, int buf_len) {
- */
-/*   unsigned char tmp[4]; */
-/*   int dst_i = 0; */
-/*   int src_y; */
-/*   int src_x;     */
-/*   /\* int v; *\/ */
-
-/*   unsigned char *dst = malloc(buf_len); */
-/*   memset(dst, 0, buf_len); */
-/*   for (src_x = width - 1; src_x >= 0; --src_x) { */
-/*     for (src_y = 0; src_y < heigth; src_y += 4) { */
-/*       tmp[0] = dd_graphic_get_pixel(src_x, src_y, width, buf, buf_len); */
-/*       tmp[1] = dd_graphic_get_pixel(src_x, src_y + 1, width, buf, buf_len);
- */
-/*       tmp[2] = dd_graphic_get_pixel(src_x, src_y + 2, width, buf, buf_len);
- */
-/*       tmp[3] = dd_graphic_get_pixel(src_x, src_y + 3, width, buf, buf_len);
- */
-
-/*       dd_graphic_set_bit(dst_i++, tmp[0], dst, buf_len); */
-/*       dd_graphic_set_bit(dst_i++, tmp[1], dst, buf_len); */
-/*       dd_graphic_set_bit(dst_i++, tmp[2], dst, buf_len); */
-/*       dd_graphic_set_bit(dst_i++, tmp[3], dst, buf_len);       */
-/*     } */
-/*   } */
-
-/*   return dst; */
-/* } */
-
-static unsigned char *dd_wvs75v2b_rotate(int width, int heigth,
+/**
+   This function is heavilly optimized, do not touch unless you know what you're doing!
+*/
+static unsigned char *dd_wvs75v2b_rotate(int width, int height,
                                          unsigned char *buf, int buf_len) {
-  unsigned char *dst = calloc(buf_len, 1);
-  for (int y = 0; y < heigth; y++) {
-    int src_row = y * width;
-    for (int x = 0; x < width; x++) {
-      int src_bit = src_row + (width - 1 - x);
-      int dst_bit = x * heigth + y;
+  unsigned char *dst = calloc(1, buf_len);
+  int src_row = 0;
+  for (int y = 0; y < height; y++) {
+    int src_bit = src_row + (width - 1);
+    int dst_bit = y;
+
+    for (int x = width - 1; x >= 0; x--) {
+
       int v = (buf[src_bit >> 3] >> (src_bit & 7)) & 1;
+
       if (v) {
-        dst[dst_bit >> 3] |= (1 << (dst_bit & 7));
+        dst[dst_bit >> 3] |= 1 << (dst_bit & 7);
       }
+
+      src_bit--;
+      dst_bit += height;
     }
+
+    src_row += width;
   }
 
   return dst;
 }
-
-
-
-/* static inline int dd_graphic_get_bit(int i, unsigned char *buf, */
-/*                                      uint32_t buf_len) { */
-/*   if (i < 0 || (uint32_t)i >= buf_len * 8) { */
-/*     return -1; */
-/*   } */
-
-/*   int byte = i >> 3; // same as i/8 but faster */
-/*   int bit = (i % 8); */
-
-/*   return (buf[byte] >> bit) & 1; */
-/* } */
-
-/* static inline void dd_graphic_set_bit(int i, int val, unsigned char *buf, */
-/*                                       uint32_t buf_len) { */
-/*   if (i < 0 || (uint32_t)i >= buf_len * 8) { */
-/*     return; */
-/*   } */
-
-/*   int byte = i >> 3; // same as i/8 but faster */
-/*   int bit = (i % 8); */
-
-/*   if (val) { */
-/*     buf[byte] |= (1u << bit); */
-/*   } else { */
-/*     buf[byte] &= ~(1u << bit); */
-/*   } */
-/* } */
-
-/* static inline int dd_graphic_get_pixel(int x, int y, int width, */
-/*                                        unsigned char *buf, uint32_t buf_len) { */
-/*   int bit = width * y + x; */
-
-/*   return dd_graphic_get_bit(bit, buf, buf_len); */
-/* } */
-
-/* static inline void dd_graphic_set_pixel(int x, int y, int v, int width, */
-/*                                         unsigned char *buf, uint32_t buf_len) { */
-/*   int bit = width * y + x; */
-
-/*   dd_graphic_set_bit(bit, v, buf, buf_len); */
-/* } */
-
-/* static unsigned char *dd_wvs75v2b_rotate(int width, int heigth, */
-/*                                          unsigned char *buf, int buf_len) { */
-/*   int v; */
-
-/*   unsigned char *dst = calloc(buf_len, 1); */
-/*   for (int y = 0; y < heigth; ++y) { */
-/*     for (int x = width - 1, x2 = 0; x >= 0; --x, x2++) { */
-/*       v = dd_graphic_get_pixel(x, y, width, buf, buf_len); */
-/*       if (v){ */
-/*         dd_graphic_set_pixel(y, x2, v, heigth, dst, buf_len); */
-/*       }         */
-/*     } */
-/*   } */
-
-/*   return dst; */
-/* } */
 
