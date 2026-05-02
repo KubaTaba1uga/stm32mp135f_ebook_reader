456266cceb965d27991882503c3abc69d538fb82
KubaTaba1uga <luna7337anul@proton.me>
2026-05-03 01:08:31 +0200

    add boot over nfs

--- board/stm32mp135f_dk/u-boot/uboot-post-rsync.sh
+++ board/stm32mp135f_dk/u-boot/uboot-post-rsync.sh
@@ -4,4 +4,4 @@ set -xeu
 BUILD_DIR="$1"
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cp "$SCRIPT_DIR/default.env" "$BUILD_DIR/board/st/stm32mp1/uboot.env"
+cp "$SCRIPT_DIR"/*.env "$BUILD_DIR/board/st/stm32mp1/"
