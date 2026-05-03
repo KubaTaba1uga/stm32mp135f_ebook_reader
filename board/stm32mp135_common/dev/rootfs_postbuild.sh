25f96ba0cc2bb03bfb6c32405d58ffdb2fd4b2ea
KubaTaba1uga <luna7337anul@proton.me>
2026-05-03 19:32:58 +0200

    add common dir from stm32mp135

--- board/stm32mp135_common/dev/rootfs_postbuild.sh
+++ board/stm32mp135_common/dev/rootfs_postbuild.sh
@@ -5,7 +5,14 @@ set -xeu
 mkdir -p ${BINARIES_DIR}/boot
 
 cp ${BINARIES_DIR}/zImage ${BINARIES_DIR}/boot/
-cp ${BINARIES_DIR}/stm32mp135f-dk-ebook-reader.dtb ${BINARIES_DIR}/boot/
+
+find ${BINARIES_DIR} -name '*-ebook-reader.dtb' | while read -r path
+do
+    cp $path ${BINARIES_DIR}/boot/ebook-reader.dtb
+done
+
+
+# find ${BINARIES_DIR} -name '*-ebook-reader.dtb' -exec cp {} ${BINARIES_DIR}/boot/ebook-reader.dtb
 
 rm -f ${BINARIES_DIR}/boot.ext2
 mke2fs -d ${BINARIES_DIR}/boot ${BINARIES_DIR}/boot.ext2 16M
--- board/stm32mp135_common/dev/rootfs_postbuild_openocd.sh
+++ board/stm32mp135_common/dev/rootfs_postbuild_openocd.sh
@@ -2,10 +2,19 @@
 
 set -xeu
 
-sh $BR2_EXTERNAL_EBK_READER_PATH/board/stm32mp135f_dk/dev/rootfs_postbuild.sh
+sh $BR2_EXTERNAL_EBK_READER_PATH/board/stm32mp135_common/dev/rootfs_postbuild.sh
 
-$HOST_DIR/bin/stm32wrapper4dbg -s ${BINARIES_DIR}/tf-a-stm32mp135f-dk-ebook-reader.stm32 -d ${BINARIES_DIR}/tf-a-stm32mp135f-dk-ebook-reader-debug.stm32 -b -f
+find ${BINARIES_DIR} -name 'tf-a-*.stm32' | while read -r path
+do
+    file_name=basename($path)
+    dst_path=${BINARIES_DIR}/${file_name}-debug.stm32
+    $HOST_DIR/bin/stm32wrapper4dbg -s $path -d $dst_path -b -f
+    cp $dst_path $path
+done
 
-cp ${BINARIES_DIR}/tf-a-stm32mp135f-dk-ebook-reader-debug.stm32 ${BINARIES_DIR}/tf-a-stm32mp135f-dk-ebook-reader.stm32
+
+# # $HOST_DIR/bin/stm32wrapper4dbg -s ${BINARIES_DIR}/tf-a-stm32mp135f-dk-ebook-reader.stm32 -d ${BINARIES_DIR}/tf-a-stm32mp135f-dk-ebook-reader-debug.stm32 -b -f
+
+# cp ${BINARIES_DIR}/tf-a-stm32mp135f-dk-ebook-reader-debug.stm32 ${BINARIES_DIR}/tf-a-stm32mp135f-dk-ebook-reader.stm32
 
 echo ${BINARIES_DIR}
