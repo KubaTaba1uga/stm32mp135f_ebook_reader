2f98a69b4750914bc201b11e1e6fb63e36448450
KubaTaba1uga <luna7337anul@proton.me>
2026-05-03 19:41:59 +0200

    fix bugs

--- board/stm32mp135_common/dev/rootfs_postbuild.sh
+++ board/stm32mp135_common/dev/rootfs_postbuild.sh
@@ -9,10 +9,14 @@ cp ${BINARIES_DIR}/zImage ${BINARIES_DIR}/boot/
 find ${BINARIES_DIR} -name '*-ebook-reader.dtb' | while read -r path
 do
     cp $path ${BINARIES_DIR}/boot/ebook-reader.dtb
+    break
 done
 
-
-# find ${BINARIES_DIR} -name '*-ebook-reader.dtb' -exec cp {} ${BINARIES_DIR}/boot/ebook-reader.dtb
+find ${BINARIES_DIR} -name '*.stm32' | while read -r path
+do
+    mv $path ${BINARIES_DIR}/tf-a-ebook-reader.stm32
+    break
+done
 
 rm -f ${BINARIES_DIR}/boot.ext2
 mke2fs -d ${BINARIES_DIR}/boot ${BINARIES_DIR}/boot.ext2 16M
--- board/stm32mp135_common/dev/rootfs_postbuild_openocd.sh
+++ board/stm32mp135_common/dev/rootfs_postbuild_openocd.sh
@@ -6,15 +6,8 @@ sh $BR2_EXTERNAL_EBK_READER_PATH/board/stm32mp135_common/dev/rootfs_postbuild.sh
 
 find ${BINARIES_DIR} -name 'tf-a-*.stm32' | while read -r path
 do
-    file_name=basename($path)
+    file_name=$(basename $path .stm32)
     dst_path=${BINARIES_DIR}/${file_name}-debug.stm32
     $HOST_DIR/bin/stm32wrapper4dbg -s $path -d $dst_path -b -f
     cp $dst_path $path
 done
-
-
-# # $HOST_DIR/bin/stm32wrapper4dbg -s ${BINARIES_DIR}/tf-a-stm32mp135f-dk-ebook-reader.stm32 -d ${BINARIES_DIR}/tf-a-stm32mp135f-dk-ebook-reader-debug.stm32 -b -f
-
-# cp ${BINARIES_DIR}/tf-a-stm32mp135f-dk-ebook-reader-debug.stm32 ${BINARIES_DIR}/tf-a-stm32mp135f-dk-ebook-reader.stm32
-
-echo ${BINARIES_DIR}
